3d397a02d61931bc842f6e0c84435cf7d93ef62c
vankhoa011 <vankhoa011@gmail.com>
2013-08-01 23:36:11 +0700

    change noi dung email forget password

--- frontend/views/mail/forgetPassword.php
+++ frontend/views/mail/forgetPassword.php
@@ -1,6 +1,6 @@
 Chào <?php echo $username; ?>,<br/>
-Bạn yêu cầu mật khẩu mới tại http://itake.me<br/>
+Bạn yêu cầu mật khẩu mới tại <a>http://itake.me</a><br/>
 Mật khẩu mới của  bạn là <?php echo $newPassword; ?><br/>
-Sau khi truy cập http://itake.me với mật khẩu mới, hãy đổi mật khẩu của bạn. <br/>
+Sau khi truy cập <a>http://itake.me</a> với mật khẩu mới, hãy đổi mật khẩu của bạn. <br/>
 Trân trọng,<br/>
 <?php echo Yii::app()->name; ?> team
