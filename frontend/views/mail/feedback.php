722a223f778f4680b8e4c700acfd3d64944210a0
root <nam.trankhanh.vn@gmail.com>
2013-08-03 10:50:14 +0700

    email template

---
+++ frontend/views/mail/feedback.php
@@ -0,0 +1,8 @@
+Chào <?php echo $adminName; ?>,<br/>
+
+Bạn vừa nhận feedback từ iTake.me với nội dung:<br/>
+Người gửi: <?php echo $username; ?><br/>
+Email: <?php echo $email; ?><br/>
+Nội dung: <?php echo $message; ?><br/>
+Ngày gửi: <?php echo $create_date; ?><br/>
+
