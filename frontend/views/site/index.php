b907883f6a3b1ffc42a45595f5fd5f2ec5960201
troynguyen <tuan.itus@gmail.com>
2013-08-07 23:56:05 +0700

    add term english

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -32,7 +32,7 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/nada/pro
             </div>    
             <div class="row-fluid">     
                 <ul>
-                    <li><a href="<?php echo Yii::app()->createUrl('site/index') ?>" title="Xem tất cả"><span class="nav-text all-cat-wrap selected mark"><small class="all-cat"></small><em></em></span></a></li>                                                      
+                    <li><a href="<?php echo Yii::app()->createUrl('site/index') ?>" title="<?php echo LanguageUtil::t('All')?>"><span class="nav-text all-cat-wrap selected mark"><small class="all-cat"></small><em></em></span></a></li>                                                      
                     <?php foreach (CategoryUtil::getCategoryList() as $category): ?>
                         <li><a href="<?php echo $category->getUrl(); ?>" title='<?php echo LanguageUtil::t($category->name)?>'><span class="nav-text <?php echo $category->getStyleName(); ?>"><small><i class="<?php echo $category->icon; ?> icon-large"></i> <em></em></small>  <?php //echo $category->name;  ?></span></a></li>
                     <?php endforeach; ?>                    
--- frontend/views/site/pages/term_en.php
+++ frontend/views/site/pages/term_en.php
@@ -8,34 +8,34 @@ $this->breadcrumbs=array(
 ?>
 <div class="container-fluid " style="margin-top:44px;">
 	<div class="span12 custom">
-		<h3 class="title-font" style="text-align:center;margin-top:15px;">ĐIỀU KHOẢN SỬ DỤNG ITAKE</h3>
+		<h3 class="title-font" style="text-align:center;margin-top:15px;"> TERMS AND CONDITIONS</h3>
 
 
-<p><strong>Quy định đăng tin tr&ecirc;n ITAKE</strong></p>
+<p><strong>Posting Rules and Conditions on ITAKE.ME:</strong></p>
 
-<p>1.&nbsp;&nbsp;&nbsp;&nbsp; Mọi kh&aacute;ch h&agrave;ng đi&ecirc;̀u c&oacute; th&ecirc;̉ đăng tin miễn ph&iacute; v&agrave; kh&ocirc;ng giới han tr&ecirc;n <strong>ITAKE</strong></p>
+<ol>
+	<li>All users have unlimited free posts on ITAKE.</li>
+	<li>Users must be responsible for their contents and information, which are posted on itake.me to comply the copyrights from other resources.</li>
+	<li>Users must post their products to the defined categories. The copyright of the posts belongs to users therefore there must be a respond for the truthfulness and accuracy of the contents of the posts.</li>
+	<li>Users do not allow posting to ITAKE.ME the following violations:
+	<ul>
+		<li>&nbsp;<em>Text with special characters.</em></li>
+		<li><em>&nbsp;Post products to wrong category.</em></li>
+		<li><em>&nbsp;Spam (Repeat same information in one or different categories).</em></li>
+		<li><em>&nbsp;Violated message (fraud, illegal, vulgar words, etc.,).</em></li>
+		<li><em>&nbsp;The post(s) contains link(s) from other sites&nbsp;</em></li>
+	</ul>
+	</li>
+</ol>
 
-<p>2.&nbsp;&nbsp;&nbsp;&nbsp; Kh&aacute;ch h&agrave;ng khi đăng tin phải chịu tr&aacute;ch nhi&ecirc;̣m v&ecirc;̀ n&ocirc;̣i dung v&agrave; th&ocirc;ng tin đăng rao vặt của m&igrave;nh tr&ecirc;n <strong>ITAKE</strong></p>
+<p><strong>ITAKE rights:</strong></p>
 
-<p>3.&nbsp;&nbsp;&nbsp;&nbsp; Kh&aacute;ch h&agrave;ng đăng tin rao vặt phải đ&uacute;ng chuy&ecirc;n mục. Bản quy&ecirc;̀n v&ecirc;̀ tin đăng thu&ocirc;̣c ch&iacute;nh kh&aacute;ch h&agrave;ng đăng tin, kh&aacute;ch h&agrave;ng đảm bảo t&iacute;nh trung thực, ch&iacute;nh x&aacute;c của n&ocirc;̣i dung tin đăng.</p>
+<ol>
+	<li>ITAKE has full rights to remove users&rsquo; post(s) if the post(s) is/are violated to the Rules and Conditions.</li>
+	<li>ITAKE does not involve in the transactions, nor the dispute between the users, nor resolve any users complaints about the posts.</li>
+	<li>We have the right to change one or more provisions contained in this rule without explaining the reasons and also without prior notice.</li>
+</ol>
 
-<p>4.&nbsp;&nbsp;&nbsp;&nbsp; Kh&ocirc;ng được đăng tin vi phạm những điều sau:</p>
-
-<ul>
-	<li>Ti&ecirc;u đ&ecirc;̀ c&oacute; k&yacute; tự đặc bi&ecirc;̣t.</li>
-	<li>Tin rao sai chuy&ecirc;n mục.</li>
-	<li>Tin rao spam (Rao nhi&ecirc;̀u tin gi&ocirc;́ng nhau).</li>
-	<li>Tin vi phạm n&ocirc;̣i quy (lừa đảo,vi phạm ph&aacute;p lu&acirc;̣t, từ ngữ th&ocirc; tục&hellip;).</li>
-	<li>Tin c&oacute; chứa đường d&acirc;̃n từ c&aacute;c website rao vặt kh&aacute;c,&hellip;</li>
-</ul>
-
-<p><strong>Quy&ecirc;̀n hạn của của ITAKE</strong></p>
-
-<p>a. &nbsp; <strong>ITAKE</strong>&nbsp;sẽ to&agrave;n quy&ecirc;̀n loại bỏ c&aacute;c tin đăng rao vặt của kh&aacute;ch h&agrave;ng n&ecirc;́u như tin đăng rao vặt vị phạm quy ch&ecirc;́ đăng tin</p>
-
-<p>b. &nbsp; <strong>ITAKE</strong>&nbsp;kh&ocirc;ng giải quy&ecirc;́t c&aacute;c tranh ch&acirc;́p giữa kh&aacute;ch h&agrave;ng , cũng như kh&ocirc;ng giải quy&ecirc;́t b&acirc;́t cứ khi&ecirc;́u nại n&agrave;o của kh&aacute;ch h&agrave;ng v&ecirc;̀ tin đăng.</p>
-
-<p>c.&nbsp;&nbsp;&nbsp;Ch&uacute;ng t&ocirc;i c&oacute; to&agrave;n quy&ecirc;̀n thay đ&ocirc;̉i m&ocirc;̣t hay nhi&ecirc;̀u đi&ecirc;̀u khoản c&oacute; trong quy định n&agrave;y m&agrave; kh&ocirc;ng c&acirc;̀n giải th&iacute;ch l&yacute; do v&agrave; cũng kh&ocirc;ng c&acirc;̀n phải th&ocirc;ng b&aacute;o trước.</p>
 
 	</div>
 </div>
