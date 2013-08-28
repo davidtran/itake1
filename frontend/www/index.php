b7bc587e7ed2dada067a520ba4a5f37494935077
root <vankhoa011@gmail.com>
2013-08-28 10:53:02 +0700

    commit index.php

--- frontend/www/index.php
+++ frontend/www/index.php
@@ -12,8 +12,8 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 // On dev display all errors
 if (YII_DEBUG) {
-//	error_reporting(-1);
-//	ini_set('display_errors', true);
+	error_reporting(-1);
+	ini_set('display_errors', true);
 }
 
 date_default_timezone_set('Asia/Ho_Chi_Minh');
