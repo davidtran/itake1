2683ef7d5c50dd1ff8210de205816665fb95178e
troynguyen <tuan.itus@gmail.com>
2013-09-24 15:49:58 +0700

    update UI city dialog

--- frontend/views/site/partial/cityDialog.php
+++ frontend/views/site/partial/cityDialog.php
@@ -2,7 +2,7 @@
     <div class="modal hide fade" id='cityDialog' style="top:50%;">        
         <div class="modal-body" > 
             <div class="row-fluid">
-                <h1><?php LanguageUtil::echoT('Please choose a city') ?></h1>
+                <h3 class="intro_font center"><?php LanguageUtil::echoT('Please choose a city') ?></h3>
             </div>
             <?php
                 $listCities = CityUtil::getCityList(true);  
@@ -10,8 +10,8 @@
             <?php foreach ( $listCities as $cityId=>$city) {
                ?>
 
-                <div class="row-fluid">
-                    <a class="btn span6 offset3"href="<?php echo Yii::app()->createUrl('site/city',array('id'=>$cityId,'category'=>''))?>"><?php echo LanguageUtil::t($city['name']); ?></a>
+                <div class="row-fluid margin-top-5">
+                    <a class="btn span6 offset3 btn-info"href="<?php echo Yii::app()->createUrl('site/city',array('id'=>$cityId,'category'=>''))?>"><?php echo LanguageUtil::t($city['name']); ?></a>
                 </div>       
                <?php
                //CityUtil::makeSelectCitychooseUrl($city),
