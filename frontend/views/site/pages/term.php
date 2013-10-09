d86a4adcfea5890454227148bf67146b926b7915
vankhoa011 <vankhoa011@gmail.com>
2013-10-09 16:59:55 +0700

    update dieu le

--- frontend/views/site/pages/term.php
+++ frontend/views/site/pages/term.php
@@ -10,27 +10,62 @@ $this->breadcrumbs=array(
 	<div class="span12 custom">
 		<h3 class="title-font" style="text-align:center;margin-top:15px;">ĐIỀU KHOẢN SỬ DỤNG ITAKE</h3>
 
-<p><strong>Quyền lợi của người MUA hàng trên iTake:</strong></p>
-<p>Người mua có quyền tìm kiếm sản phẩm theo nhu cầu của mình từ những sản phẩm được post mới nhất, gần nơi mình sống nhất, những sản phẩm bạn bè trên facebook đang bán hay những sản phẩm đang quan tâm.</p>
-<p>Người mua có quyền đánh giá, thảo luận về một sản phẩm với chức năng bấm like, và nhận xét về những sản phẩm mình quan tâm. Ngoài ra người mua có quyền báo xấu về một sản phẩm quảng cáo không đúng chất lượng, vi phạm các quy định làm xấu đi uy tín đối với iTake.</p>
-<p><strong>Quy định người BÁN hàng trên iTake</strong></p>
-
-<p>1.&nbsp;&nbsp;&nbsp;&nbsp; Mọi doanh nghiệp hay cá nhân kinh doanh đều có thể đăng tin miễn phí và xây dựng thương hiệu sản phẩm của mình trên iTake.</p>
-
-<p>2.&nbsp;&nbsp;&nbsp;&nbsp; Người bán hàng được tham khảo ý kiến của dùng về sản phẩm đang bán để thay đổi chất lượng tốt hơn.</p>
-
-<p>3.&nbsp;&nbsp;&nbsp;&nbsp; Người bán hàng khi đăng tin phải chịu trách nhiệm về nội dung và thông tin đăng rao vặt của mình trên iTake.</p>
-
-<p>4.&nbsp;&nbsp;&nbsp;&nbsp; Nhằm đảm bảo chất lượng bài đăng, mỗi người chỉ đăng 5 tin / ngày</p>
-
-<p>5.&nbsp;&nbsp;&nbsp;&nbsp; Không được đăng tin lừa đảo, chất lượng sản phẩm sai với thông tin quảng cáo, trái với thuần phong mỹ tục Việt Nam, vi phạm pháp luật Việt Nam.</p>
-
-<p><strong>Quy&ecirc;̀n hạn của của iTake</strong></p>
-
-<p>a. &nbsp; iTake sẽ toàn quyền loại bỏ các tin đăng rao vặt của người bán nếu như tin đăng rao vặt vị phạm quy chế đăng tin như: Sai quy định của iTake, bị nhiều báo xấu từ người mua.</p>
-
-<p>b. &nbsp; Ban quản trị  iTake không có thẩm quyền  giải quyết các tranh chấp giữa người mua và người bán, chỉ xem xét xử lý những hành động phá hoại iTake như lừa gạt, hack, bug, … .</p>
-
+<p><strong style="font-size:16px">Quy định Bán Hàng</strong>:</p>
+   
+  <ul class="Icon"><li>Mọi khách hàng được đăng tối đa 3 sản phẩm / 1 ngày / 1 tài  khoản.&nbsp;Việc tạo nhiều tài khoản để đăng tin là nghiêm cấm.</li>
+    <li>
+      Khách hàng khi đăng tin  phải chịu trách nhiệm về nội dung và thông tin đăng rao vặt của mình đối với  iTake 
+    </li>
+    <li>
+      Khách hàng đăng tin rao vặt phải đúng chuyên mục. Khách hàng đảm bảo tính trung  thực, chính xác của nội dung tin đăng. 
+    </li>
+    <li>Không văng&nbsp;tục,&nbsp;lăng mạ lẫn nhau&nbsp;khi trao đổi thông tin  với nhau trên iTake
+    </li>
+	
+	</ul>
+<li><strong>Không được đăng tin vi phạm những điều sau:</strong>
+      <ul>
+        <li>Tiêu đề̀, mô tả sản phẩm  có ký tự đặc biệt gây khó đọc, ảnh hưởng đến thiết kế iTake. </li>
+        <li>Tin rao sai chuyên mục. </li>
+        <li>Tin rao spam (Rao nhiều tin  giống nhau). </li>
+        <li>
+         Tin vi phạm nội quy (lừa  đảo,vi phạm pháp luật, từ ngữ thô tục…). 
+        </li>
+        <li>
+          Tin có hình ảnh chất lượng  thấp, ảnh hưởng đến thiết kế của iTake. 
+        </li>
+        <li>
+          Tin lừa đảo, chất lượng sản  phẩm sai với thông tin quảng cáo, trái với thuần phong mỹ tục Việt Nam, vi phạm  pháp luật Việt Nam. 
+        </li>
+      </ul>
+  <br>
+</li>
+<p><strong style="font-size:16px">Quyền lợi mua hàng:</strong></p>
+<ul class="Icon">
+  <li>
+   Người mua có quyền tìm  kiếm sản phẩm theo nhu cầu của mình từ những sản phẩm được post mới nhất, gần  nơi mình sống nhất, những sản phẩm bạn bè trên facebook đang bán hay những sản  phẩm đang quan tâm. 
+  </li>
+  <li>
+    Người mua có quyền đánh  giá, thảo luận về một sản phẩm với chức năng bấm like, và nhận xét về những sản  phẩm mình quan tâm. 
+  </li>
+  <li>
+  Ngoài ra người mua có quyền báo xấu về một sản phẩm quảng  cáo không đúng chất lượng, vi phạm các quy định làm xấu đi uy tín đối với  iTake. 
+  </li>
+</ul><br />
+<p><strong style="font-size:16px">Quyền hạn của iTake:</strong></p>
+<ul class="Icon">
+  <li>
+    iTake sẽ toàn quyền  loại bỏ các tin đăng rao vặt của khách hàng mà không cần báo trước nếu như tin  đăng rao vặt vị phạm quy chế đăng tin.
+  </li>
+  <li>
+  iTake có  quyền khoá tài khoản của thành viên vi phạm nội quy.
+  </li>
+  <li>
+   iTake không chịu trách nhiệm hay khiếu nại giao dịch giữa  người bán với người mua.</li>
+  <li>iTake không tham gia vào  giao dịch của người bán và người mua và cũng không chịu bất kỳ trách nhiệm nào  phát sinh từ việc sử dụng iTake.
+  </li>
+   <li>iTake  có toàn quyền thay đổi một hay nhiều điều khoản có trong quy định này mà không cần giải thích lý do và thông báo trước. </li>
+</ul>
 	</div>
 </div>
 
