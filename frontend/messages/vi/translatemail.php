afcfd8956b3600659ce43b411ed29da6347dd779
vankhoa011 <vankhoa011@gmail.com>
2013-10-10 23:48:37 +0700

    dich email

---
+++ frontend/messages/vi/translatemail.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+    
+    //Change password
+    'Dear'=>'Chào',
+    'You have just changed your password for'=>'Bạn vừa mới thay đổi mật khẩu cho',
+    'ITAKE ACCOUNT'=>'tài khoản iTake',
+    'We would like to confirm that your change is properly.'=>'Chúng tôi xác nhận bạn đã thay đổi mật khẩu thành công',
+    'If it was changed by others or cause a security problem, please let us know, you could send an email to support@itake.me'=>'Nếu mật khẩu của bạn thay đổi bởi người khác hay một lý do nào đó, xin vào lòng báo cho chúng tôi qua địa chỉ mail support@itake.me',
+    'Best Regards' => 'Thân ái',
+    
+    //Forget password
+    'You have just requested a new password for ITAKE ACCOUNT. Please enter the auto generated password below when you sign in ITAKE next time' => 'Bạn đã yêu cầu đổi mật khẩu cho tài khoản iTake, vui lòng đăng nhập mật khẩu này vào lần đăng nhập tiếp theo',
+    'After you sign in' => 'Sau khi bạn đăng nhập',
+    'please change password by your new password' => ' Vui lòng thay đổi mật khẩu',
+    'If you get any query during change the password, you could send an email to support@itake.me.' => 'Nếu gặp bất cứ khó khăn trong việc thay đổi password, vui lòng liên lạc với chúng tôi thông qua địa chỉ email sau support@itake.me',
+    
+    //Message
+    'You have got a new message from' => 'Bạn vừa nhận được một tin nhắn từ',
+    
+    //Analysis
+    'This email is sumary of result social share,view and like report. It will help you get better in your business' => 'Đây là mail thống kê số lượt xem, chia sẻ, và yêu thích sản phẩm của bạn. Chức năng này sẽ giúp các bạn định hướng tốt cho việc kinh doanh của bạn',
+    'If your have any query, feel free to ask via' => 'Nếu có bất kỳ yêu cầu nào, vui lòng liên lạc với chúng tôi thống qua'
+    
+
+);
