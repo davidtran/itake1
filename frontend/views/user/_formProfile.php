5dc5aadc6051b69b45eee51557674e4532a408d6
troynguyen <tuan.itus@gmail.com>
2013-10-13 00:53:26 +0700

    update

--- frontend/views/user/_formProfile.php
+++ frontend/views/user/_formProfile.php
@@ -4,51 +4,53 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
 
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 	'id'=>'user-_formProfile-form',
 	'enableAjaxValidation'=>false,
+    'type'=>'vertical',
+    'htmlOptions'=>array('class'=>'form','role'=>'form'),
 )); ?>
 
 	<!-- <p class="note">Fields with <span class="required">*</span> are required.</p>
  -->
+    <fieldset>
 	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->textFieldRow($model,'email'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->textFieldRow($model,'username'); ?>
-	</div>
-
-	<div class="row">
+        <div class="control-group">
+		    <?php echo $form->textFieldRow($model,'email',array('disabled'=>true,'class'=>'controls','labelOptions'=>array('class'=>'control-label'))); ?>
+        </div>
+        <div class="control-group">
+		    <?php echo $form->textFieldRow($model,'username',array('class'=>'controls','labelOptions'=>array('class'=>'control-label'))); ?>
+        </div>
+        <div class="control-group">
 		 <?php echo $form->datepickerRow(
             $model,
             'birthday',
             array(
                 'options' => array('format' => 'dd/mm/yyyy' , 'weekStart'=> 1),
-                'prepend' => '<i class="icon-calendar"></i>'
+                'class'=>'controls',
+                'labelOptions'=>array('class'=>'control-label'),
+                'prependOptions'=>array('class'=>'span2')
             )
         ); ?>
-	</div>
+        </div>
 
+        <div class="control-group">
+		    <?php echo $form->textFieldRow($model,'locationText',array('class'=>'controls','labelOptions'=>array('class'=>'control-label'))); ?>
+        </div>
 
-	<div class="row">
-		<?php echo $form->textFieldRow($model,'locationText'); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->textFieldRow($model,'phone'); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->textFieldRow($model,'target'); ?>
-	</div>
+        <div class="control-group">
+		    <?php echo $form->textFieldRow($model,'phone',array('class'=>'controls','labelOptions'=>array('class'=>'control-label'))); ?>
+        </div>
 
-	<div class="row buttons">
+        <div class="control-group">
+		    <?php echo $form->textAreaRow($model,'target',array('rows'=>3,'class'=>'controls','labelOptions'=>array('class'=>'control-label'))); ?>
+         </div>
+
+	<div class="buttons">
 		<?php echo CHtml::submitButton(LanguageUtil::t('Update'),array('class' => 'btn btn-success')); ?>
 	</div>
-
+    </fieldset>
 <?php $this->endWidget(); ?>
 
-</div><!-- form -->
+<!-- form -->
